a946cfc43b4725231dea2f54fba0f128e4bbbec4
Zachary Wright Heller <zheller@gmail.com>
2026-04-20 14:42:08 -0400

    Update keybindings
    go back to tpd
    fix some zsh vi mode weirdness and make fancy cursor

--- dot_config/zsh/aliases.zsh
+++ dot_config/zsh/aliases.zsh
@@ -10,4 +10,4 @@ alias cze="chezmoi edit"
 alias cza="chezmoi apply"
 alias cat='bat --plain --paging=never --color=always'
 alias lg=lazygit
-alias tmuxpld='tmuxp load default -s "$(basename "$(git rev-parse --show-toplevel 2>/dev/null || pwd)")"'
+alias tpd='tmuxp load default -s "$(basename "$(git rev-parse --show-toplevel 2>/dev/null || pwd)")"'
